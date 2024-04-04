293d08f1efa18262608a8e1f69321c982f768701
Zombie1995 <egorkerber1995@mail.ru>
2024-04-04 21:01:08 +0300

    Horizontal scroll + adjust font for archetype

--- src/entities/archetype/ui/index.tsx
+++ src/entities/archetype/ui/index.tsx
@@ -7,13 +7,14 @@ interface Props {
 
 export const ArchetypeCard = ({ title, description }: Props) => {
   return (
-    <button className="relative lg:min-w-[30svw] lg:w-[30svw] sm:min-w-[50svw] sm:w-[50svw] min-w-[80svw] w-[508vw] h-[100svh] shadow-md flex flex-col justify-center">
+    <button className="relative lg:min-w-[30svw] lg:w-[30svw] sm:min-w-[50svw] sm:w-[50svw] min-w-[80svw] w-[80vw] h-[100svh] shadow-md flex flex-col justify-center">
       <div className="z-[-1] bg-[url('https://catherineasquithgallery.com/uploads/posts/2021-03/1614724659_55-p-foni-dlya-lendinga-62.jpg')] bg-cover absolute top-0 w-full h-full">
         <AnimatedBackground />
       </div>
       <div className="absolute top-[45%] p-4 text-left">
-        <p className="font-medium text-[36px] text-white">{title}</p>
-        <p className="whitespace-pre-wrap text-white break-words">
+        <p className="font-medium text-[36px] text-white leading-9">{title}</p>
+        <div className="h-[10px]" />
+        <p className="whitespace-pre-wrap text-[12px] text-white break-words">
           {description}
         </p>
       </div>
--- src/widgets/scroll-wrapper/ui/index.tsx
+++ src/widgets/scroll-wrapper/ui/index.tsx
@@ -1,11 +1,45 @@
-import { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 
 export const ScrollWrapper = ({ children }: { children: ReactNode }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [clickedChild, setClickedChild] = useState(-1);
+
+  const scrollToChild = (index: number) => {
+    const childElement = scrollRef.current?.children[index];
+    if (childElement) {
+      childElement.scrollIntoView({ behavior: "auto", inline: "start" });
+    }
+  };
+
+  useEffect(() => {
+    if (clickedChild > -1) {
+      scrollToChild(clickedChild);
+      setTimeout(() => {
+        setClickedChild(-1);
+      }, 1000);
+    }
+  }, [clickedChild]);
+
   return (
     <div className="relative flex flex-col h-[100svh] overflow-hidden">
       <div className="z-[100] absolute top-0 left-[50svw] origin-top-left w-[30svw] h-[40svh] bg-gradient-to-t from-primary to-secondary rounded-b-full scale-x-[5] translate-y-[-20svh] -translate-x-[250%]" />
-      <div className="flex h-full items-center overflow-x-scroll no-scrollbar gap-[20px] px-[20px]">
-        {children}
+      <div
+        ref={scrollRef}
+        className="flex h-full items-center overflow-x-scroll no-scrollbar gap-[20px] pl-[20px]"
+      >
+        {React.Children.map(children, (child, index) => (
+          <div key={index} onClick={() => setClickedChild(index)}>
+            {child}
+          </div>
+        ))}
+        <div>
+          {clickedChild > -1 && <div className="min-w-[80svw] w-[80svw]" />}
+          <div
+            className={`${
+              clickedChild > -1 ? "min-w-[80svw] w-[80svw]" : "min-w-0 w-0"
+            } transition-width duration-500 ease-in-out`}
+          />
+        </div>
       </div>
       <div className="z-[100] absolute bottom-0 left-[50svw] origin-top-left w-[30svw] h-[40svh] bg-gradient-to-b from-primary to-secondary rounded-t-full scale-x-[5] translate-y-[20svh] -translate-x-[250%]" />
     </div>
