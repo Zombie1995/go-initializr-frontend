ba74afafc4a999e169f40c1209d6b7dd01a9c487
Zombie1995 <egorkerber1995@mail.ru>
2024-04-08 18:02:01 +0300

    Download button + light theme card button fix

---
+++ src/features/download-archetype/index.ts
@@ -0,0 +1,3 @@
+export * as DownloadArchetypeApi from "./api";
+export * as DownloadArchetypeModel from "./model";
+export * from "./ui";
---
+++ src/features/download-archetype/ui/index.tsx
@@ -0,0 +1,22 @@
+export const DownloadArchetype = () => {
+  return (
+    <button className="size-[256px]">
+      <svg
+        className="fill-[#21ac4a] dark:fill-[#a1f859]"
+        width="256"
+        height="256"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M18 15V18H6V15H4V18C4 19.1 4.9 20 6 20H18C19.1 20 20 19.1 20 18V15H18ZM17 11L15.59 9.59L13 12.17V4H11V12.17L8.41 9.59L7 11L12 16L17 11Z"
+          fill="current"
+        />
+      </svg>
+      <p className="font-medium text-[24px] text-[#21ac4a] dark:text-[#a1f859] -translate-y-10">
+        Скачать
+      </p>
+    </button>
+  );
+};
